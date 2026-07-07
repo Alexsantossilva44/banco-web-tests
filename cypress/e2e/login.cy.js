5f75d37213b95d500e4a13a5e3ef2d7edfe31a78
Alex Silva <alexsantossilva44@gmail.com>
2026-07-07 22:38:53 +0100

    implementando comandos customizaveis para Login, Transferecias e Commons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,35 +1,21 @@
 describe('Login', () => {
-
   beforeEach(() => {
-
     // Arrange
     cy.visit('/')
     cy.screenshot('após-visitar-a-página')
-
   })
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act
-    cy.fixture('credenciais').then(cred => {
-      cy.get('#username').click().type(cred.valida.usuario)
-      cy.get('#senha').click().type(cred.valida.senha)
-    })
-    cy.contains('button', 'Entrar').click()
-
+    cy.fazerLoginComCredenciaisValidas()
     // Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve exibir mensagem de erro', () => {
     // Act
-    cy.fixture('credenciais').then(cred => {
-      cy.get('#username').click().type(cred.invalida.usuario)
-      cy.get('#senha').click().type(cred.invalida.senha)
-    })
-    cy.get('#login-section > .btn').click()
-
+    cy.fazerLoginComCredenciaisInvalidas()
     // Assert
-    // cy.get('.toast').should('be.visible')
-    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.')
   })
 })
---
+++ cypress/e2e/transferencia.cy.js
@@ -0,0 +1,16 @@
+describe('Transferencia', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.fazerLoginComCredenciaisValidas()
+    })
+
+    it('Deve transferir quando informo dados válidos', () => {
+        cy.realizarTransferencia('Maria', 'João', '11')
+        cy.verificarMensagemNoToast('Transferência realizada!')
+    })
+
+    it('Deve apresentar erro quando tentar transferir mais que 5 mil sem o token', () => {
+        cy.realizarTransferencia('Maria', 'João', '5001')
+        cy.verificarMensagemNoToast('Autenticação necessária para transferências acima de R$5.000,00.')
+    })
+})
